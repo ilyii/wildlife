3a6884f92675e3c013d9a3a96350b7a0d734b9c7
ilyii <ihettenbach@gmx.net>
2023-10-14 20:56:40 +0200

    Refine the clustering algorithm.

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+
+class Config:
+    # Data
+    datapath = 'D:\Database\\animals\images'
+    labelfile = "D:\Database\\animals\\name of the animals.txt"
+
+    # Params
+    img_size = 224
+
+
